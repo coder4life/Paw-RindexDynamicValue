d3befe277ef997cf7d54e3b18e1ffc61ab5eafbc
coder4life <coder4life.dev@gmail.com>
2017-07-19 15:10:07 -0700

    Fix some bugs and adding percentage lookup
    Simplified Regex
    Fixed a big on root node fetch to detect if it is a returned JSONObject vs a JSONArray.
    Removed uneeded dependencies.
    Added percentage based lookup for random values.

--- src/RindexDynamicValue.js
+++ src/RindexDynamicValue.js
@@ -4,7 +4,6 @@
 
     var entities = require('html-entities').AllHtmlEntities,
         get = require('lodash.get'),
-        isInteger = require('lodash.isinteger'),
         random = require('lodash.random'),
         size = require('lodash.size');
 
@@ -19,57 +18,87 @@
     };
 
     var getPath = function(source, keypath) {
-        var regexValue = new RegExp("[A-Za-z0-9]*\\[([0-9]+|[\\*]+|[\\@f]+|[\\@l]+|[\\@m]+)\\]");
-        var regexReplace = new RegExp("\\*|\\@f|\\@l|\\@m");
+        var regexArrayFormatKeyCapture = new RegExp("[A-Za-z0-9]*\\[([0-9]+|[\\*]|\\@[flm]|\\@p\\:)\\]");
+        var regexArrayFormatKeyRemove = new RegExp("\\[.*?\\]");
         var pathItems = keypath.split('.');
         var pathList = [];
         var result;
 
-        for(var i in pathItems) {
-            if (pathItems.hasOwnProperty(i)) {
-                var regexTest = regexValue.test(pathItems[i]);
-                if (regexTest) {
-                    var regexResult = regexValue.exec(pathItems[i]);
-                    var length = getLength(source, pathList, pathItems[i]);
-                    if (regexResult[1] === '*') {
-                        var rIndex = random(length - 1);
-                        pathList.push(pathItems[i].replace(regexReplace, rIndex));
-                    } else if(regexResult[1] === '@f') {
-                        pathList.push(pathItems[i].replace(regexReplace, 0));
-                    }
-                    else if(regexResult[1] === '@l') {
-                        pathList.push(pathItems[i].replace(regexReplace, length - 1));
-                    }
-                    else if(regexResult[1] === '@m') {
-                        pathList.push(pathItems[i].replace(regexReplace, Math.round((length - 1)/2)));
-                    }
-                    else if (isInteger(parseInt(regexResult[1]))) {
-                        pathList.push(pathItems[i]);
-                    } else {
-                        throw "Error with your array syntax key[value], please check...";
-                    }
-                } else {
-                    pathList.push(pathItems[i]);
+        console.log('pathItems: ' + pathItems);
+        console.log('pathList: ' + pathList);
+
+        for(var i = 0; i < pathItems.length; i++) {
+
+            var regexArrayFormatKeyCaptureTest = regexArrayFormatKeyCapture.test(pathItems[i]);
+
+            // pathItem returns a formatted structure, do some operation on it based on the function
+            if (regexArrayFormatKeyCaptureTest) {
+                var regexArrayFormatKeyCaptureResult = regexArrayFormatKeyCapture.exec(pathItems[i]);
+                var length = getLength(source, pathList, pathItems[i].replace(regexArrayFormatKeyRemove, ''));
+
+                if (regexArrayFormatKeyCaptureResult[1] === '*') {
+                    var rIndex = random(length - 1);
+                    pathList.push(
+                        pathItems[i].replace("*", rIndex)
+                    );
+                } else if(regexArrayFormatKeyCaptureResult[1] === '@f') {
+                    pathList.push(
+                        pathItems[i].replace("@f", 0)
+                    );
                 }
+                else if(regexArrayFormatKeyCaptureResult[1] === '@l') {
+                    pathList.push(
+                        pathItems[i].replace("@l", length - 1)
+                    );
+                }
+                else if(regexArrayFormatKeyCaptureResult[1] === '@m') {
+                    pathList.push(
+                        pathItems[i].replace("@m", Math.round((length - 1) * 0.5))
+                    );
+                }
+                else if(regexArrayFormatKeyCaptureResult[1] === '@p:') {
+                    var regexPositionValue = new RegExp("\\@p\\:(\d{1,2}(?!\d)|100)");
+                    var regexPositionValueTest = regexPositionValue.test(pathItems[regexArrayFormatKeyCaptureResult[1]]);
+                    if(regexPositionValueTest) {
+                        var regexPositionValueResult = regexPositionValue.exec(regexArrayFormatKeyCaptureResult[1]);
+                        pathList.push(
+                            pathItems[i].replace(
+                                regexPositionValue, Math.round((length - 1) * (regexPositionValueResult[1] / 100))
+                            )
+                        );
+                    }
+                }
+                else if (Number.isInteger(parseInt(regexArrayFormatKeyCaptureResult[1]))) {
+                    pathList.push(pathItems[i]);
+                } else {
+                    throw "Error with your array syntax key[value], please check...";
+                }
+
+            } else { // do not do anything, not a special format, just push current key item
+                pathList.push(pathItems[i]);
             }
+
         }
         result = pathList.join('.');
         return result;
     };
 
     var getLength = function(source, pathList, pathItem) {
-        var regex = new RegExp("([A-Za-z0-9]*)\\[[0-9]+|[\\*]+|[\\@f]+|[\\@l]+|[\\@m]+\\]");
-        var path = pathList.join('.');
-        var regexResult = regex.exec(pathItem);
-        var lastRequest = getLastRequest(source);
-        var result;
 
-        if(path && regexResult[1]) { // [x].path.to.pathItem
-            result = size(get(lastRequest, path + '.' + regexResult[1]));
-        } else if(path) { // [x].pathItem
-            result = size(get(lastRequest, pathItem));
-        } else { // [x]
-            result = size(lastRequest);
+        var path = pathList.join('.');
+
+        var lastRequest = getLastRequest(source);
+        var result = null;
+
+        if(path) { // key[x].path.to[x].pathItem
+            result = size(get(lastRequest, path + '.' + pathItem));
+        } else { // key[x]
+            console.log("isArray: " + Array.isArray(lastRequest));
+            if(Array.isArray(lastRequest)) {
+                result = size(lastRequest);
+            } else {
+                result = size(get(lastRequest, pathItem))
+            }
         }
         if(!result) {
             throw "Error key was not found"
@@ -77,7 +106,7 @@
         return result;
     };
 
-// Extensions are implemented as JavaScript classes
+    // Extensions are implemented as JavaScript classes
     var RindexDynamicValue = function() {
 
         // implement the evaluate() method to generate the dynamic value
@@ -110,22 +139,22 @@
 
     var request = null;
 
-// set the Extension Identifier (must be same as the directory name)
+    // set the Extension Identifier (must be same as the directory name)
     RindexDynamicValue.identifier = "com.coder4life.PawExtensions.RindexDynamicValue";
 
-// give a display name to your Dynamic Value
+    // give a display name to your Dynamic Value
     RindexDynamicValue.title = "Rindex";
 
-// link to the Dynamic Value documentation
+    // link to the Dynamic Value documentation
     RindexDynamicValue.help = "https://github.com/coder4life/Paw-RindexDynamicValue";
 
-// inputs
+    // inputs
     RindexDynamicValue.inputs = [
         InputField("source", "Source Request", "Request"),
         InputField("keypath", "KeyPath", "String")
     ];
 
-// call to register function is required
+    // call to register function is required
     registerDynamicValueClass(RindexDynamicValue);
 
 }).call(this);
